5bf5e3a7bafb04666b8f263d8145cca3fc6f06d0
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-02-28 23:09:23 +0200

    js-home-3

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const lazyImages = document.querySelectorAll('[loading="lazy"]');
-lazyImages.forEach((image) => {
-    image.addEventListener("load", (events) => {
-        events.target.classList.add("show");
-        console.log("test")
-    }), { once: true }
+const imagesArr = document.querySelectorAll('.img')
+const observer = new IntersectionObserver(observerImages)
 
+function loadImage(image) {
+    const dataSrc = image.getAttribute('data-src')
+    image.src = dataSrc
+    image.classList.add('animation-image')
+}
+
+function observerImages(imagesArr) {
+    imagesArr.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            loadImage(entry.target)
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+imagesArr.forEach((imageEl) => {
+    observer.observe(imageEl)
+    imageEl.classList.remove('animation-image')
 })
